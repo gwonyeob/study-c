deb7c283cf45465efdce1e4b982c067da712455e
apircity <peter261104@naver.com>
2022-05-06 02:03:17 +0900

    별 피라미드 완성

--- 7주차 과제 3번 별 피라미드.c
+++ 7주차 과제 3번 별 피라미드.c
@@ -2,11 +2,17 @@
 int main()
 {
 	int num = 0;
+	int gap = 0; //필요한 공백 수
 	printf("줄 수: ");
 	scanf_s("%d", &num);
+	gap = num - 1;
 	int count = 1; //피라미드의 길이
 	for (int i = 1; i <= num; i++)
 	{
+		for (int i = 1; i <= gap; i++)
+		{
+			printf(" ");
+		}
 		for (int j = 1; j <= count; j++)
 		{
 			if (j % 2 == 1)
@@ -17,6 +23,7 @@ int main()
 				printf("\n");
 		}
 		count += 2;
+		gap -= 1;
 	}
 	return 0;
 }
